652d2c1bce8d3c4b5f1120994351707c32b87f92
Arky <20231736nbsc.edu.ph>
2025-02-20 15:24:54 +0800

    Started practice

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,14 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Ready to create an app?</strong>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default ExploreContainer;
---
+++ src/pages/Details.tsx
@@ -0,0 +1,44 @@
+import { 
+  IonButtons,
+    IonContent, 
+    IonHeader,  
+    IonPage, 
+    IonTitle, 
+    IonToolbar,
+    IonCard, 
+    IonCardContent, 
+    IonCardHeader, 
+    IonCardSubtitle, 
+    IonCardTitle,
+    IonButton
+} from '@ionic/react';
+
+const Details: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot='start'>
+          </IonButtons>
+          <IonTitle>Details</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+      <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
+
+      <IonCardContent>
+      <h1>Hey there I love you</h1>
+      <IonButton>click me</IonButton>
+      <IonButton disabled={true}>Disabled</IonButton>
+      </IonCardContent>
+    </IonCard>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Details;
